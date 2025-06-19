65516c8606ae380587cb8a7b7db1bf6639ee078e
rod <matveev.r.v@icloud.com>
2025-06-19 20:21:27 +0300

    task-4

---
+++ src/ts/task-4.ts
@@ -0,0 +1,37 @@
+interface Refs {
+  form: HTMLFormElement | null;
+}
+
+const refs: Refs = {
+  form: document.querySelector<HTMLFormElement>(".login-form")!,
+};
+
+refs.form?.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const target = e.target as HTMLFormElement | null;
+  if (!target) {
+    return;
+  }
+
+  const dataObj: Record<string, string> = {};
+
+  const formData = new FormData(target);
+  formData.forEach((value, key) => {
+    const val = value as string;
+    const validatedValue = validateInput(val);
+    if (validatedValue) dataObj[key] = validatedValue;
+  });
+  console.log(dataObj);
+  target.reset();
+}
+
+function validateInput(value: string): string {
+  const EMPTY_PLACEHOLDER = "n/a";
+  if (value.trim() === "") {
+    alert("The value must not be empty");
+    return EMPTY_PLACEHOLDER;
+  }
+  return value;
+}
